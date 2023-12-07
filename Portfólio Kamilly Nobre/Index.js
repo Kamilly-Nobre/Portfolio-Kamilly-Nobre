d19fe9eb001844985e130bcb889aea4ecd14c83d
Kamilly-Nobre <118058389+Kamilly-Nobre@users.noreply.github.com>
2023-12-06 21:42:15 -0300

    Delete Portfólio Kamilly Nobre directory

--- Portfólio Kamilly Nobre/Index.js
+++
@@ -1,9 +0,0 @@
-let header = document.getElementById('header')
-        
-            window.addEventListener('scroll', () => {
-        if(window.scrollY >= 500){
-            header.style.background = 'rgb(7, 7, 36)'
-        } else {
-        header.style.background = 'transparent'
-        }
-        })
